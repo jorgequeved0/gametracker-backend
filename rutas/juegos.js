90f5524cd0ec68c48e677c8b6646e8be74974c66
jorgqueved0 <jq335488@gmail.com>
2025-11-05 10:52:41 -0500

    Correcion de errores y agrego manejo de errores con try/catch en todos los endpoints

--- rutas/juegos.js
+++ rutas/juegos.js
@@ -5,33 +5,49 @@ const Review = require('../modelos/Review');
 
 // Metodo Get
 router.get('/', async (req, res) => {
-    const juegos = await Juego.find();
-    res.json(juegos);
+    try {
+        const juegos = await Juego.find();
+        res.json(juegos);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los juegos' });
+    }
 });
 
 // Metodo post
 router.post('/', async (req, res) => {
-    const nuevoJuego = new Juego(req.body);
-    const juegoGuardado = await nuevoJuego.save();
-    res.status(201).json(juegoGuardado);
+    try {
+        const nuevoJuego = new Juego(req.body);
+        const juegoGuardado = await nuevoJuego.save();
+        res.status(201).json(juegoGuardado);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al guardar el juego'});
+    }
 });
 
 // Metodo put
 router.put('/:id', async (req, res) => {
-    const juegoActualizado= await Juego.findByIdAndUpdate(
+    try {
+       const juegoActualizado= await Juego.findByIdAndUpdate(
         req.params.id, 
         req.body, 
         { new: true },
-    )
-    res.json(juegoActualizado);
+        );
+        res.json(juegoActualizado); 
+    } catch (error) {
+        res.status(500).json({ error: 'Error al actualizar el juego' });
+    }
 });
 
 // Metodo delete
 router.delete('/:id', async (req, res) => {
-    await Juego.findByIdAndDelete(
-        req.params.id,
-    );
-    res.json({ mensaje: 'Juego Eliminado' });
+    try {
+        await Juego.findByIdAndDelete(
+            req.params.id,
+        );
+        res.json({ mensaje: 'Juego Eliminado' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar  el juego' });
+    }
 });
 
 module.exports = router;
--- rutas/reviews.js
+++ rutas/reviews.js
@@ -4,32 +4,49 @@ const Review = require('../modelos/Review');
 
 // Metodo get
 router.get('/', async (req, res) => {
-    const reviews = await Review.find();
-    res.json(reviews);
+    try {
+        const reviews = await Review.find();
+        res.json(reviews);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener las reseñas' });
+    }
 });
 
 // Metodo post 
 router.post('/', async (req, res) => {
-    const nuevaReview = new Review(req.body);
-    const reviewGuardada = await nuevaReview.save();
-    res.status(201).json(reviewGuardada);
+    try {
+        const nuevaReview = new Review(req.body);
+        const reviewGuardada = await nuevaReview.save();
+        res.status(201).json(reviewGuardada);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al guardar la reseña' });
+    }
 });
 
 // Metodo put
 router.put('/:id', async (req, res) => {
-    const reviewActualizada = await Review.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true },
-    );
+    try {
+        const reviewActualizada = await Review.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true },
+        );
+        res.json(reviewActualizada);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al actualizar la reseña'})
+    }
 });
 
 // Metodo delete
-router.put('/:id', async (req, res) => {
-    await Review.findByIdAndDelete(
-        req.params.id,
-    );
-    res.json({ mensaje: 'Reseña Eliminada' });
+router.delete('/:id', async (req, res) => {
+    try {
+        await Review.findByIdAndDelete(
+            req.params.id,
+        );
+        res.json({ mensaje: 'Reseña Eliminada' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al eliminar la reseña' });
+    }
 });
 
 module.exports = router;
